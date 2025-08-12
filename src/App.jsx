5e01dfbbf427609f7cb47cf2d7c25786d444fc4f
Hi 👋, I'm Mohammad Asim <muhammadasim4927@gmail.com>
2025-08-12 22:11:57 +0500

    You commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-     <h1 className='heading'>Ci - CD pipelines</h1>
+     <h1 className='heading'>Ci - CD pipelines {process.env.VITE_PROJECT}</h1>
     </>
   )
 }
